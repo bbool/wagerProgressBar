5ac3f854b2d256e2939320c1381e3310da4ca5ec
bbool <bastien.tafforeau@gmail.com>
2021-01-17 16:03:36 +0100

    Adding casino logos on the home page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,46 @@
-import logo from './logo.svg';
+import lucky8 from './lucky8.svg';
+import casinoextra from './casino-extra.svg';
+import fatboss from './fatboss.png';
+import stake from './stake.svg';
+import tortuga from './tortuga.png';
+import 'tailwindcss/tailwind.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <div id="containerLogos" className="flex container">
+          <div className="grid gap-5 grid-cols-5 place-content-center">
+            <div className="flex justify-center items-center">
+              <a href="#">
+                <img src={lucky8} className="casino-logo" alt="logo" />
+              </a>
+            </div>
+            <div className="flex justify-center items-center">
+              <a href="#">
+                <img src={casinoextra} className="casino-logo" alt="logo" />
+              </a>
+            </div>
+            <div className="flex justify-center items-center">
+              <a href="#">
+                <img src={fatboss} className="casino-logo" alt="logo" />
+              </a>
+            </div>
+            <div className="flex justify-center items-center">
+              <a href="#">
+                <img src={stake} className="casino-logo" alt="logo" />
+              </a>
+            </div>
+            <div className="flex justify-center items-center">
+              <a href="#">
+                <img src={tortuga} className="casino-logo" alt="logo" />
+              </a>
+            </div>
+          </div>
+        </div>
+        <br />
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
